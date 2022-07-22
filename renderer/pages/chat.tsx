747de026bd8fab3a0427a287080abcb511208b88
Applehole <skyheo123@naver.com>
2022-07-22 22:37:02 +0900

    Feat: chat data add

--- renderer/pages/chat.tsx
+++ renderer/pages/chat.tsx
@@ -1,50 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../style/chat.module.css'
 import NaviBar from '../Components/NaviBar'
 import UserList from '../Components/UserList'
-import { collection, onSnapshot, query, orderBy, getFirestore, doc, updateDoc } from "firebase/firestore"
+import { collection, onSnapshot, query, orderBy, getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth";
 import Router from 'next/router';
 
 function chat() {
   const [userList, setUserList] = useState([])
-
-  useEffect(()=>{
-
+  const [userData, setUserData] = useState([])
+  const auth = getAuth();
+  useEffect(() => {
     const dbService = getFirestore();
     const q = query(
       collection(dbService, "userOnline"),
       orderBy("createdAt", "desc")
     );
-    const snapAsync = async ()=>{
+    const snapAsync = async () => {
       onSnapshot(q, async (snapshot) => {
         const userArray = snapshot.docs.map((doc) => ({
           id: doc.id,
           userId: doc.id,
           online: doc.id,
-          displayName : doc.id,
+          displayName: doc.id,
           ...doc.data(),
         }));
         setUserList(userArray)
+        const currentUserArray = userArray.filter((user)=>{
+          return user.userId === auth.currentUser?.uid
+        })
+        setUserData(currentUserArray)
       })
     }
     snapAsync()
-  },[])
+  }, [])
 
-  const createChat = () =>{
+  const createChat = () => {
     Router.push("chatList")
   }
 
   return (
     <div className={styles.ChatCover}>
       <NaviBar />
-      <div>
-        <div>여기는 채팅룸입니다</div>
-        <button onClick={createChat}> 새로운 채팅 만들기 </button>
+      <div className={styles.ChatMainCover}>
+        <div className={styles.ChatMain}>
+          <h2>Chatting Room</h2>
+          <button className={styles.ChatCreateButton} onClick={createChat}> 새로운 채팅 만들기 </button>
+          {userData.length ?<div></div> :<div className={styles.ChatNoon}>새로운 채팅을 만들어주세요!</div>}
+        </div>
       </div>
       <div className={styles.userListCover}>
-      <UserList userList={userList} />
+        <UserList userList={userList} />
       </div>
     </div>
   );
