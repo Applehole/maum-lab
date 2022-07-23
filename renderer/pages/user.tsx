399a983915e5755f3f06d8e81dd6dd3842b17571
Applehole <skyheo123@naver.com>
2022-07-23 21:38:27 +0900

    Feat: user Css

--- renderer/pages/user.tsx
+++ renderer/pages/user.tsx
@@ -7,67 +7,69 @@ import { collection, onSnapshot, query, orderBy, getFirestore, doc, updateDoc } 
 
 
 function user() {
-    const auth = getAuth()
-    const [userName, setUserName] = useState(auth.currentUser)
-    const [input, setInput] = useState("")
-    const [userList, setUserList] = useState([])
+  const auth = getAuth()
+  const [userName, setUserName] = useState(auth.currentUser)
+  const [input, setInput] = useState("")
+  const [userList, setUserList] = useState([])
 
-    useEffect(()=>{
+  useEffect(() => {
 
-      const dbService = getFirestore();
-      const q = query(
-        collection(dbService, "userOnline"),
-        orderBy("createdAt", "desc")
-      );
-      const snapAsync = async ()=>{
-        onSnapshot(q, async (snapshot) => {
-          const userArray = snapshot.docs.map((doc) => ({
-            id: doc.id,
-            userId: doc.id,
-            online: doc.id,
-            ...doc.data(),
-          }));
-          setUserList(userArray)
-        })
-      }
-      snapAsync()
-    },[])
-
-    console.log("userList",userList)
-
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-      setInput(e.target.value)
+    const dbService = getFirestore();
+    const q = query(
+      collection(dbService, "userOnline"),
+      orderBy("createdAt", "desc")
+    );
+    const snapAsync = async () => {
+      onSnapshot(q, async (snapshot) => {
+        const userArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          userId: doc.id,
+          online: doc.id,
+          ...doc.data(),
+        }));
+        setUserList(userArray)
+      })
     }
+    snapAsync()
+  }, [])
 
-    const onSubmit = async (e: React.FormEvent) =>{
-      e.preventDefault();
-      if(input !== auth.currentUser?.displayName && input !== auth.currentUser?.uid){
-        await updateProfile(auth.currentUser,{
-          displayName : input
-        })
-        setInput("")
-      }
+  console.log("userList", userList)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input !== auth.currentUser?.displayName && input !== auth.currentUser?.uid) {
+      await updateProfile(auth.currentUser, {
+        displayName: input
+      })
+      setInput("")
     }
+  }
 
   return (
     <div className={styles.userCover}>
-    { userName ?
-    <>
-      <NaviBar />
-      <div>
-        <div>유저 공간이야!</div>
-        <form onSubmit={(e)=>onSubmit(e)}>
-        <input type="text" onChange={(e)=>onChange(e)} value={input} placeholder={auth.currentUser?.displayName|| auth.currentUser?.uid}></input>
-        <input type="submit" value="유저 아이디 변경하기" ></input>
-        </form>
-        <div>유저이름 </div>
-      </div>
-      <div className={styles.userListCover}>
-       <UserList userList={userList} />
-      </div>
-    </>
-    :<div>다시 로그인 하셔야 할듯한데요?</div>
-    }
+      {userName ?
+        <>
+          <NaviBar />
+          <div className={styles.userProfileCover}>
+            <h2 className={styles.userProfileTitle}>프로필</h2>
+            <form className={styles.userProfileForm} onSubmit={(e) => onSubmit(e)}>
+              <div className={styles.userProfileFormName}>유저이름 : {auth.currentUser?.displayName || auth.currentUser?.uid}</div>
+              <div className={styles.userProfileFormDiv}>
+              <input className={styles.userProfileFormInput} type="text" onChange={(e) => onChange(e)} value={input} placeholder={auth.currentUser?.displayName || auth.currentUser?.uid}></input>
+              <input className={styles.userProfileFormButton} type="submit" value="유저 아이디 변경하기" ></input>
+              </div>
+            </form>
+          </div>
+          <div className={styles.userListCover}>
+            <UserList userList={userList} />
+          </div>
+        </>
+        : <div>다시 로그인 해야합니다.</div>
+      }
     </div>
   );
 };
