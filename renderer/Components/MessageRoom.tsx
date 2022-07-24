f445652a40d55c8f8118d09d7fcaaaee552710d4
Applehole <skyheo123@naver.com>
2022-07-24 23:36:52 +0900

    Feat: chatting Room delete

--- renderer/Components/MessageRoom.tsx
+++ renderer/Components/MessageRoom.tsx
@@ -12,6 +12,7 @@ function MessageRoom({ user }) {
                     <div key={el.id} onClick={directToChat} className={styles.messageCover} >
                         <div>채팅방이름 : {String(el).slice(2)}</div>
                         <div>참여자 이름 : {user.userId}</div>
+                        <button>삭제버튼</button>
                     </div>
                 )
             })
