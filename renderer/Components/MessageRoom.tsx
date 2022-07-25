3c9d69164daaf9089f53376bb58b89c6d25541ef
Applehole <skyheo123@naver.com>
2022-07-25 21:46:09 +0900

    Feat: Delete chattingRoom

--- renderer/Components/MessageRoom.tsx
+++ renderer/Components/MessageRoom.tsx
@@ -1,18 +1,31 @@
 import styles from '../style/messageRoom.module.css'
 import Router from 'next/router';
+import { doc, updateDoc, getFirestore } from "firebase/firestore";
 
 function MessageRoom({ user }) {
+    const dbService = getFirestore();
+    const ChatRef = doc(dbService, "userOnline", `${user.id}`);
+
     return (
         <div >{
             user.chat.map((el) => {
                 const directToChat = () => {
                     Router.push(`${el}`)
                 }
+                const onClick = async (e) =>{
+                    e.stopPropagation()
+                    let filteredChattingRoom = user.chat.filter((each)=>{
+                        return each !== el
+                    })
+                    await updateDoc(ChatRef,{
+                        chat: filteredChattingRoom
+                      });
+                }
                 return (
                     <div key={el.id} onClick={directToChat} className={styles.messageCover} >
                         <div>채팅방이름 : {String(el).slice(2)}</div>
                         <div>참여자 이름 : {user.userId}</div>
-                        <button>삭제버튼</button>
+                        <button onClick={(e)=>onClick(e)} className={styles.messageDelete}>삭제</button>
                     </div>
                 )
             })
